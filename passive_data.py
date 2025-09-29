ff1a783e14cff6278915ab0fee78a13cc3f89ee2
Lizzie <emmastrangelo@wpi.edu>
2025-09-29 14:42:44 -0400

    Beginning to set up passive data analysis

--- passive_data.py
+++ passive_data.py
@@ -10,11 +10,14 @@ screentime_general.rename(columns={'id':'screentime_id'}, inplace=True)
 screentime_merged = screentime_app.merge(screentime_general, on='screentime_id')
 
 # Import and append associated PHQ-9 scores
-phq9 = pd.read_csv('lemurs-ml/data/question_202509261602.csv')
+question = pd.read_csv('lemurs-ml/data/question_202509261602.csv')
+# Select only PHQ-9 questions
+# phq9 = question[question['Survey' == 'PHQ9']] # Need to make Survey column first
+# ACTUALLY I think we can just join it with Survey_question. Can check when DB is back up
 
 print(screentime_merged.columns)
 print(screentime_merged.head())
-print(phq9.columns)
+# print(phq9.columns)
 
 # Feature engineering
 
