64219b75af42dc0dc902648a8e50504c3062a144
Lizzie <emmastrangelo@wpi.edu>
2025-09-29 13:06:18 -0400

    Beginning to set up passive data analysis

---
+++ passive_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Import passive df
+screentime_app = pd.read_csv('lemurs-ml/data/passive/screentime_app_data.csv')
+screentime_app.rename(columns={'id':'screentime_app_id'}, inplace=True)
+screentime_general = pd.read_csv('lemurs-ml/data/passive/screentime_data.csv')
+screentime_general.rename(columns={'id':'screentime_id'}, inplace=True)
+
+# Connect screentime_app and screentime_all on screentime_id
+screentime_merged = screentime_app.merge(screentime_general, on='screentime_id')
+
+# Import and append associated PHQ-9 scores
+phq9 = pd.read_csv('lemurs-ml/data/question_202509261602.csv')
+
+print(screentime_merged.columns)
+print(screentime_merged.head())
+print(phq9.columns)
+
+# Feature engineering
+
+#
