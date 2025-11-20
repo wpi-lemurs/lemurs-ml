09617686218508d038ddadde07e9e2ee16cf55ac
jjmurphy1114 <jjmurphy1114@outlook.com>
2025-11-20 08:57:33 -0500

    Changed import of new merge_weekly_health file

--- src/steps_modeling.py
+++ src/steps_modeling.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_auc_score
-from merge_weekly_steps_with_phq9 import merge_weekly_health_with_phq9
+from merge_weekly_health_with_phq9 import merge_weekly_health_with_phq9
 
 modeling_data = merge_weekly_health_with_phq9()
 
